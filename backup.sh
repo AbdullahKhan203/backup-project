860452256ff1605cee0d6ebbd019177681651a35
AbdullahKhan203 <hk7485966@gmail.com>
2026-06-23 08:12:26 +0000

    Add backup script and sample data

---
+++ backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SOURCE="./data"
+DEST="./backups"
+LOG="./backup.log"
+
+mkdir -p "$DEST"
+
+TIME=$(date +"%Y-%m-%d_%H-%M-%S")
+FILE="$DEST/backup_$TIME.tar.gz"
+
+tar -czf "$FILE" "$SOURCE"
+
+if [ $? -eq 0 ]; then
+    echo "$TIME Backup SUCCESS" >> "$LOG"
+    echo "Backup done"
+else
+    echo "$TIME Backup FAILED" >> "$LOG"
+fi
