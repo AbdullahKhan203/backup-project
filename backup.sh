89ad7d587c4603649f526dd552c7b995efbf25aa
AbdullahKhan203 <hk7485966@gmail.com>
2026-06-23 13:12:21 +0000

    Backup #9 completed at 2026-06-23_13-12-21

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ mkdir -p backups
 # 2. Timestamp
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 
-# 3. Backup number (auto increment)
+# 3. Backup number system
 count_file="backups/count.txt"
 
 if [ ! -f "$count_file" ]
@@ -21,29 +21,43 @@ count=$(cat "$count_file")
 # 4. Create backup
 tar -czf backups/backup_${count}_${timestamp}.tar.gz data
 
-# 5. Check result
+# 5. Check backup success
 if [ $? -eq 0 ]
 then
     echo "$(date): Backup #$count successful - backup_${count}_${timestamp}.tar.gz" >> backup.log
 
-    # increase counter
-    count=$((count + 1))
-    echo $count > "$count_file"
+    # increment counter
+    next_count=$((count + 1))
+    echo $next_count > "$count_file"
 
     # =========================
-    # 🔥 GIT AUTO PUSH SECTION
+    # 🔥 GIT AUTO SYNC FIXED
     # =========================
 
-    git add backups backup.log backups/count.txt
+    # Add ONLY important files (ignore rule safe)
+    git add data backup.sh backups/count.txt
 
+    # Commit with auto number
     git commit -m "Backup #$count completed at $timestamp"
 
+    # Push to GitHub
     git push origin main
 
 else
     echo "$(date): Backup #$count failed" >> backup.log
     exit 1
 fi
+
+
+
+
+
+
+
+
+
+
+
 
 
 
