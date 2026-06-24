6918fb8618ab38dcb5e04673184174a2a73a250f
AbdullahKhan203 <hk7485966@gmail.com>
2026-06-24 20:40:14 +0500

    Backup #20

--- backup.sh
+++ backup.sh
@@ -2,15 +2,10 @@
 
 echo "Starting backup + git automation..."
 
-# 1. Create backup folder
 mkdir -p backups
 
-# 2. Timestamp
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 
-# =========================
-# 🔥 GET BACKUP NUMBER FROM GIT HISTORY
-# =========================
 
 last_num=$(git log --oneline | grep "Backup #" | head -1 | grep -o "#[0-9]*" | grep -o "[0-9]*")
 
@@ -21,7 +16,6 @@ else
     count=$((last_num + 1))
 fi
 
-# 3. Create backup
 tar -czf backups/backup_${count}_${timestamp}.tar.gz data
 
 # 4. Check result
